da48f6121ed8731388da5d0436f913a14f692189
unknown <bmargerison@outlook.com>
2021-09-11 12:28:45 +0100

    to work from laptop

--- ex44.py
+++ ex44.py
@@ -34,6 +34,7 @@ Scene
 """
 
 from sys import exit
+from random import randint
 
 class Room(object):
 
@@ -57,16 +58,15 @@ class Motor(object):
 
 class Dead(Room):
 
-	def enter(self):
-		quips = [
-		"You died. You kinda suck at this.",
-		"Your mom would be proud... if she were smarter.",
-		"Such a luser",
-		"I have a small puppy that's better at this."
-		]
+	quips = [
+	"You died. You kinda suck at this.",
+	"Your mom would be proud... if she were smarter.",
+	"Such a luser",
+	"I have a small puppy that's better at this."
+	]
 
 	def enter(self):
-		print(Death.quips[randint(0,len(self.quips)-1)])
+		print(Dead.quips[randint(0,len(self.quips)-1)])
 		exit(1)
 
 class Atrium(Room):
